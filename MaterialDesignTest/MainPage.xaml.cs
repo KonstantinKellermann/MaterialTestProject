661fc05253a18a33f5007a67912d65e9f1f7cd59
Konstantin Kellermann <konstantin.kellermann@gmx.de>
2021-01-15 12:13:12 +0100

    Add files via upload

---
+++ MaterialDesignTest/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MaterialDesignTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MaterialTestPage());
+        }
+    }
+}
---
+++ MaterialDesignTest/MaterialTestPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MaterialDesignTest
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MaterialTestPage : ContentPage
+    {
+        public MaterialTestPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
